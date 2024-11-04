203fdabd1b123a25c04ff3add49f106c781e0265
sniemann <sascha.niemann66@gmx.de>
2024-11-04 19:59:06 +0100

    add multiple games logic

--- app/_pages/game.py
+++ app/_pages/game.py
@@ -17,9 +17,13 @@ class Game:
         if "animal" not in st.session_state:
             st.session_state["animal"] = self.random_animal()
         if "chat_history" not in st.session_state:
-            st.session_state["chat_history"] = [
-                {"role": "assistant", "content": "What is your first guess?"}
+            st.session_state["chat_history"] = [self.initial_message()]
+        if "game_history" not in st.session_state:
+            st.session_state["game_history"] = [
+                self.empty_game(st.session_state.animal)
             ]
+        if "state" not in st.session_state:
+            st.session_state["state"] = "guessing"
         self.layout()
 
     def random_animal(self) -> str:
@@ -39,9 +43,10 @@ class Game:
         Update the game state based on whether the guess is correct or incorrect.
         """
         st.session_state.chat_history.append(
-            {"role": "user", "content": st.session_state["guess"]}
+            {"role": "user", "content": st.session_state.guess}
         )
-        if st.session_state["guess"] == st.session_state["animal"]:
+        st.session_state.game_history[-1]["number_of_guesses"] += 1
+        if st.session_state.guess == st.session_state.animal:
             self.win()
         else:
             self.wrong()
@@ -51,12 +56,41 @@ class Game:
         st.session_state.chat_history.append(
             {"role": "assistant", "content": "Correct! You won."}
         )
+        st.session_state.state = "win"
 
     def wrong(self):
         """Update session state to indicate an incorrect guess."""
         st.session_state.chat_history.append(
             {"role": "assistant", "content": "Wrong! Do you have another idea?"}
         )
+
+    def reset_game(self):
+        """Reset game and initialize all necessary variables."""
+        st.session_state.animal = self.random_animal()
+        st.session_state.game_history.append(self.empty_game(st.session_state.animal))
+        st.session_state.chat_history = [self.initial_message()]
+        st.session_state.state = "guessing"
+
+    def empty_game(self, animal: str) -> dict:
+        """Get a new game history entry.
+
+        Args:
+            animal (str): the new animal to guess
+
+        Returns:
+            dict: game history entry
+
+        """
+        return {"animal": animal, "number_of_guesses": 0}
+
+    def initial_message(self) -> dict:
+        """Get initital message.
+
+        Returns:
+            dict: initial message that should be displayed at the top of the chat
+
+        """
+        return {"role": "assistant", "content": "What is your first guess?"}
 
     def layout(self):
         """Set up the game layout in the Streamlit app.
@@ -69,7 +103,14 @@ class Game:
         for message in st.session_state.chat_history:
             st.chat_message(message["role"]).write(message["content"])
 
-        st.chat_input("Enter animal:", on_submit=self.evaluate_input, key="guess")
+        st.chat_input(
+            "Enter animal:",
+            on_submit=self.evaluate_input,
+            key="guess",
+            disabled=(st.session_state.state == "win"),
+        )
+        if st.session_state.state == "win":
+            st.button("Play again", on_click=self.reset_game)
 
 
 # Instantiate and start the game
