ed8dfb46755da5099c0fb441b18fa72f5e998f05
Your Name <dnoske@uni-osnabrueck.de>
2024-11-18 17:22:48 +0100

    Fixed some weird game master behaviour and warnings

--- app/_pages/stats.py
+++ app/_pages/stats.py
@@ -7,7 +7,7 @@ import yaml
 
 st.title("Stats")
 
-stream = open("app/guessing_config.yaml", 'r')
+stream = open("app/guessing_config.yaml", "r")
 data = yaml.load(stream, Loader=yaml.Loader)
 animals = data["animals"]
 animals.insert(0, "All")
@@ -16,14 +16,21 @@ option = st.selectbox("Animals", animals)
 if "game_history" in st.session_state and len(st.session_state.game_history) > 0:
     relevant_games = st.session_state.game_history[:-1]
     if option != "All":
-        relevant_games = list(filter(lambda game: (game["animal"] == option), relevant_games))
+        relevant_games = list(
+            filter(lambda game: (game["animal"] == option), relevant_games)
+        )
     guesses = [game["number_of_guesses"] for game in relevant_games]
-    average = np.average(guesses)
-    
+    average = np.average(guesses) if len(guesses) > 0 else np.nan
+
     st.caption(f"Total games played: {len(relevant_games)}")
     st.caption(f"Average guesses: {average}")
     if len(relevant_games) > 0:
-        st.bar_chart(data=relevant_games, x="animal", y="number_of_guesses", y_label="Number of guesses")
+        st.bar_chart(
+            data=relevant_games,
+            x="animal",
+            y="number_of_guesses",
+            y_label="Number of guesses",
+        )
         moving_averages = uniform_filter(guesses, size=5, mode="nearest", output=float)
         print(guesses)
         print(moving_averages)
